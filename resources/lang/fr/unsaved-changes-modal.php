0839d7f65bf3016e52bee8940edcbe2ee2d41940
Olin <45310038+OlinB@users.noreply.github.com>
2026-09-10 08:38:08 +0200

    Add french translation

---
+++ resources/lang/fr/unsaved-changes-modal.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'navigation' => [
+        'heading' => 'Modifications non sauvegardées',
+        'body' => 'Vous avez des modifications non sauvegardées. Êtes-vous sûr de vouloir quitter cette page ?',
+        'stay' => 'Rester sur la page',
+        'leave' => 'Quitter sans enregistrer',
+    ],
+
+];
